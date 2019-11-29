da6674d6ebf3fa5e76260782ff9124bbbd7117b0
Jose <55567254+Josedair@users.noreply.github.com>
2019-11-28 21:38:48 -0800

    all files needed for tomato prefab

---
+++ Tomato/TOMbullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TOMbullet : MonoBehaviour
+{
+    public Transform player;
+
+    [SerializeField]
+    float bulletSpeed = 5f;
+    Rigidbody2D rb;
+    float finalSpeed;
+
+    GameObject playerObject;
+    Vector3 currentPlayerPos;
+    Vector2 moveDirection;
+
+    public SpriteRenderer sauce;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        moveDirection = (playerObject.transform.position - transform.position).normalized * bulletSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 1f);
+
+        if (GameObject.FindGameObjectWithTag("Player").transform.localPosition.x - transform.localPosition.x <= 0)
+        {
+            sauce.flipX = true;
+        }
+        else sauce.flipX = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Destroy(gameObject, 1.5f);
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Tomato/tomateAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tomateAI : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 45f;
+    public float chaseRange;
+    public float shootRange;
+    public SpriteRenderer tomateBoss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").transform.localPosition.x - transform.localPosition.x <= 0)
+        {
+            tomateBoss.flipX = true;
+        }
+        else tomateBoss.flipX = false;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < chaseRange)
+        {
+            Vector3 direction = -(player.position - transform.position);
+            direction.Normalize();
+            movement = direction;
+            moveEnemy(movement);
+        }
+
+        //shootRange must be greater than chaseRange or else tomate will vibrate
+        else if(distanceToPlayer > shootRange)
+        {
+            Vector3 direction = (player.position - transform.position);
+            direction.Normalize();
+            movement = direction;
+            moveEnemy(movement);
+        }
+    }
+
+
+    void moveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
