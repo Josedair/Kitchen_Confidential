3a586d5918e402b909fed1b67faff90a39c0dc8a
Jose <55567254+Josedair@users.noreply.github.com>
2019-12-04 02:45:46 -0800

    the potato needs an enemy character script

---
+++ POTdeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POTdeath : MonoBehaviour
+{
+    public Transform spawnpoint1;
+    public Transform spawnpoint2;
+    public GameObject potato1;
+    public GameObject potato2;
+    public float expirationTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        expirationTime = Time.time + 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= expirationTime)
+        {
+            potDeath();
+        }
+    }
+
+    public void potDeath()
+    {
+        Instantiate(potato1, spawnpoint1.position, spawnpoint1.rotation);
+        Instantiate(potato2, spawnpoint2.position, spawnpoint2.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ PotatoAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoAI : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 45f;
+    public float chaseRange;
+    
+    public SpriteRenderer potatoBoss;
+    // Start is called before the first frame update
+    void Start()
+    {
+         player = GameObject.FindWithTag("Player");
+    rb = this.GetComponent<Rigidbody2D>();
+        //player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").transform.localPosition.x - transform.localPosition.x <= 0)
+        {
+            potatoBoss.flipX = true;
+        }
+        else potatoBoss.flipX = false;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer < chaseRange)
+        {
+            Vector3 direction = (player.transform.position - transform.position);
+            direction.Normalize();
+            movement = direction;
+            moveEnemy(movement);
+        }
+    }
+
+    void moveEnemy(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
